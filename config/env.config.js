b30fb5b7bc2fb3e2e4c887517352efffd99734e3
Muh. Shamad SST <shamad2402@gmail.com>
2020-09-14 17:05:24 +0800

    add anak pkl

--- config/env.config.js
+++ config/env.config.js
@@ -39,5 +39,32 @@ module.exports = {
             [1,0,'tipe2'], //jumat
             [0,0,'tipe2'], //sabtu
         ],
+        'SMK_Nur Wahyuningsih': [
+            [0,0,'tipe2'], //minggu
+            [1,0,'tipe2'], //senin
+            [1,0,'tipe2'], //selasa
+            [1,0,'tipe2'], //rabu
+            [1,0,'tipe2'], //kamis
+            [1,0,'tipe2'], //jumat
+            [0,0,'tipe2'], //sabtu
+        ],
+        'SMK_Nurhaifa': [
+            [0,0,'tipe2'], //minggu
+            [1,0,'tipe2'], //senin
+            [1,0,'tipe2'], //selasa
+            [1,0,'tipe2'], //rabu
+            [1,0,'tipe2'], //kamis
+            [1,0,'tipe2'], //jumat
+            [0,0,'tipe2'], //sabtu
+        ],
+        'SMK_Yulianti': [
+            [0,0,'tipe2'], //minggu
+            [1,0,'tipe2'], //senin
+            [1,0,'tipe2'], //selasa
+            [1,0,'tipe2'], //rabu
+            [1,0,'tipe2'], //kamis
+            [1,0,'tipe2'], //jumat
+            [0,0,'tipe2'], //sabtu
+        ]
     }
 }
